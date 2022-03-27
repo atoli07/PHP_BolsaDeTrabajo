5850ac10d8e7de9757d01fbb7185653d97954b75
Jonnathan-UDB <90229545+Jonnathan-UDB@users.noreply.github.com>
2022-03-26 21:51:52 -0600

    datos para la bdd
    datos de empresa, ofertas, detalles ofertas.

---
+++ bdd/datos.sql
@@ -0,0 +1,11 @@
+INSERT INTO empresas VALUES
+("10001","VALORES Y SERVICIOS REGIONALES","usuario@vsr.sv.cv","12345678"),
+("10002","ALMACENES SIMAN","usuario@siman.sv.cv","12345678"),
+("10003","POLLO CAMPERO","usuario@pollo.campero.sv.cv","12345678"),
+("10004","TIGO","usuario@tigo.sv.cv","12345678"),
+("10005","DON POLLO","usuario@don.pollo.sv.cv","12345678"),
+("10006","RH SOLUCIONES","usuario@rh.sv.cv","12345678"),
+("10007","PAE","usuario@pae.sv.cv","12345678"),
+("10008","ASEI","usuario@asei.sv.cv","12345678"),
+("10009","PANADERIA EL ROSARIO","usuario@per.sv.cv","12345678"),
+("10010","BANCO CUSCATLÁN","usuario@bc.sv.cv","12345678")
