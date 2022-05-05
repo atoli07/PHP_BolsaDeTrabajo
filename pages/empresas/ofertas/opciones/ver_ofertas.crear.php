96319ea4af247abf32e7121d19d42778cc431509
Jonnathan-UDB <90229545+Jonnathan-UDB@users.noreply.github.com>
2022-05-05 14:55:21 -0600

    Update ver_ofertas.crear.php

--- pages/empresas/ofertas/opciones/ver_ofertas.crear.php
+++ pages/empresas/ofertas/opciones/ver_ofertas.crear.php
@@ -1,60 +1,3 @@
-<<<<<<< Updated upstream
-<form method="post">
-    <div class="container">
-        <h1 class=" text-center alert-primary">DATOS DE LA OFERTA</h1>
-        <br>
-        <label class="alert-primary">Nombre Oferta: </label>
-        <input type="text" name="NombreOferta" id="NombreOferta" class="form-control" placeholder="Ingeniería de sistemas" minlength="5" maxlength="30" required>
-
-        <br>
-        <label class="alert-primary">Descripción: </label>
-        <textarea id="descripcion" name="descripcion" class="form-control" placeholder="Ingrese descripción de la Ofertas" required></textarea>
-
-        <br>
-        <label class="alert-primary">Estado:</label>
-        <select id="Estado" name="Estado" class="form-control">
-            <option value="1">DISPONIBLES</option>
-            <option value="0">NO DISPONIBLES</option>
-        </select>
-
-        <br>
-        <label class="alert-primary">Area Empresa:</label>
-        <input type="text" name="AreaEmpresa" id="AreaEmpresa" class="form-control" placeholder="Servicios, Telecomunicaciones, etc." minlength="5" maxlength="40" required>
-
-        <br>
-        <label class="alert-primary">Cargo Solicitado:</label>
-        <input type="text" name="CargoSolicitado" id="CargoSolicitado" class="form-control" placeholder="Asistente Contable, Cocinero, etc." minlength="5" maxlength="40" required>
-
-        <br>
-        <label class="alert-primary">TipoContratacion :</label>
-        <input type="text" name="TipoContratacion" id="TipoContratacion" class="form-control" placeholder="Tiempo Completo" minlength="5" maxlength="15" required>
-
-        <br>
-        <label class="alert-primary">Educación Minima :</label>
-        <input type="text" class="form-control" id="EducacionMinima" name="EducacionMinima" placeholder="Bachillerato" minlength="5" maxlength="15" required>
-
-        <br>
-        <label class="alert-primary">Nivel Experencia:</label>
-        <input type="text" class="form-control" id="NivelExperencia" name="NivelExperencia" placeholder="sin experiencia" minlength="5" maxlength="30" required>
-
-        <br>
-        <label class="alert-primary">Edad:</label>
-        <input type="numeric" class="form-control" id="Edad" name="Edad" placeholder="0" min="18" max="100" required>
-
-        <br>
-        <label class="alert-primary">Salario Minimo:</label>
-        <input type="numeric" class="form-control" id="SalarioMin" name="SalarioMin" placeholder="$" min="0" max="1000" required>
-
-        <br>
-        <label class="alert-primary">Salario Maximo:</label>
-        <input type="numeric" class="form-control" id="SalarioMax" name="SalarioMax" placeholder="$" min="0" max="1000" required>
-
-        <br>
-        <input class='btn btn-primary btn-block' type="submit" name="guardarOferta" value="GUARDAR OFERTA" />
-
-    </div>
-</form>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -131,16 +74,17 @@
             
         </div>
     </form>
->>>>>>> Stashed changes
 <?php
 
-if (isset($_POST['guardarOferta'])) {
-    include_once("Funciones/conexión/BDD.php");
+if(isset($_POST['guardarOferta'])) {
+    include_once("../../../../Funciones/conexión/BDD.php");
 
     $NombreOferta = $_POST['NombreOferta'];
-    $Descripcion = $_POST['descripcion'];
+    $Descripcion =$_POST['descripcion'];
     $Estado = $_POST['Estado'];
 
+    $EmpresaUsuario = $_SESSION['usuario'];
+        
     $AreaEmpresa = $_POST['AreaEmpresa'];
     $CargoSolicitado = $_POST['CargoSolicitado'];
     $TipoContratacion = $_POST['TipoContratacion'];
@@ -159,17 +103,18 @@ if (isset($_POST['guardarOferta'])) {
     //incremento
     $numeroID = substr($idUltimaOferta, -3);
     $numeroID = $numeroID + 1;
-<<<<<<< Updated upstream
-    $letrasOfertas = 'OFE' . $numeroID;
-    $letrasDetallesOfertas = 'DOF' . $numeroID;
-=======
     $letrasOfertas = 'OFE'.$numeroID;
     $letrasDetallesOfertas = 'DOF'.$numeroID;
     
->>>>>>> Stashed changes
     //empresacreation
+    $EmpresaId = "SELECT* FROM ofertas ORDER BY ofertas.IdOferta DESC LIMIT 1";
+    $CopnsultaEmpresaId = $BDD_DSS->query($EmpresaId);
+    $filaConsulta = $CopnsultaEmpresaId->fetch_assoc();
+
+    $xd s
+
     $idempresa = 10001;
-    //xd
+            //xd
     echo $letrasOfertas;
 
     $IngresardatosOfertaDetalles = "INSERT INTO detallesoferta(IdDetallesOferta, AreaEmpresa, CargoSolicitado, TipoContratacion, EducacionMinima, NivelExperencia, Edad, SalarioMin, SalarioMax) VALUES
@@ -180,16 +125,9 @@ if (isset($_POST['guardarOferta'])) {
 
     $datosDetalles = $BDD_DSS->query($IngresardatosOfertaDetalles);
     $datosOfertas = $BDD_DSS->query($IngresardatosOferta);
-
-    if ($datosOfertas == TRUE) {
+            
+    if ($datosOfertas == TRUE){
         echo 'La oferta: ' . $NombreOferta . ' a sido guardado';
-<<<<<<< Updated upstream
-    } else {
-        echo "Error: " . $sql . "<br>" . $BDD_DSS->error;
-    }
-
-    header("location:empresas_ver_ofertas.php");
-=======
         header("location:../table.ver_ofertas.php");    
     }
     else{
@@ -197,6 +135,7 @@ if (isset($_POST['guardarOferta'])) {
     }
 
     
->>>>>>> Stashed changes
 }
+
 ?>
+</html>
